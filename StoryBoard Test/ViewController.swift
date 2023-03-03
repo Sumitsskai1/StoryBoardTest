a50b459b7b45e87dfa082a3560428eb077690b8a
kaifuchu1buil <masaki_sekiguchi@smtss.jp>
2023-03-03 13:45:12 +0900

    Initial Commit

---
+++ StoryBoard Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryBoard Test
+//
+//  Created by SuMiTS&S 開発第一部 on 2023/03/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
